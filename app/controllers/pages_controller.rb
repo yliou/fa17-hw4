d73d41d0d09aa5b35c6253db2a6e63aa35ad39af
yliou <yliou@berkeley.edu>
2017-10-11 15:37:01 -0700

    finished hw4

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,22 @@
 class PagesController < ApplicationController
   def home
+    @person=Todo.new()
     @data = Todo.all
     @data1 =Cat.all
     @data2 =User.all
-    @Pose=Todo.new()
+    
 
     
   end
   def create
-    person = Todo.new()
-    person.tasks=params[:tasks]
-    person.finished=params[:finished]
-    person.save
+    
+    person = Todo.new(tasks: params[:todo][:tasks], finished: params[:todo][:finished])
+    
+    person.save!
     redirect_to root_url
   end
+  private 
+    def todo_params
+      params.require(:Todo).permit(:tasks, :finished)
+end
 end
