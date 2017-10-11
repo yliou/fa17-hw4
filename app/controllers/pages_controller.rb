11156ae92067a6925bd2e3c5ed91ba86921e46ed
yliou <yliou@berkeley.edu>
2017-10-11 11:27:51 -0700

    need to post inserted form data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,15 @@ class PagesController < ApplicationController
     @data = Todo.all
     @data1 =Cat.all
     @data2 =User.all
-    end
+    @Pose=Todo.new()
+
+    
+  end
+  def create
+    person = Todo.new()
+    person.tasks=params[:tasks]
+    person.finished=params[:finished]
+    person.save
+    redirect_to root_url
+  end
 end
